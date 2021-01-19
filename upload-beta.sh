bb763f4a1a99b0b71bdf6dd90309f3913670764a
Bernardo Bas <bbas@siu.edu.ar>
2021-01-19 12:32:35 -0300

    Beta builds only for h872

--- upload-beta.sh
+++ upload-beta.sh
@@ -8,13 +8,13 @@
 # rm lineage_us997-ota*.zip *.md5sum
 # sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
-# cd /mnt/backup/android/builds/lineage/h872/
-# rm *.*
-# cd /mnt/backup/android/los17/out/target/product/h872/
-# mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h872/
-# cd /mnt/backup/android/builds/lineage/h872/
-# rm lineage_h872-ota*.zip *.md5sum
-# sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
+cd /mnt/backup/android/builds/lineage/h872/
+rm *.*
+cd /mnt/backup/android/los17/out/target/product/h872/
+mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h872/
+cd /mnt/backup/android/builds/lineage/h872/
+rm lineage_h872-ota*.zip *.md5sum
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
 # cd /mnt/backup/android/builds/lineage/h870ds/
 # rm *.*
@@ -24,13 +24,13 @@
 # rm lineage_h870ds-ota*.zip *.md5sum
 # sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
-cd /mnt/backup/android/builds/lineage/h870/
-rm *.*
-cd /mnt/backup/android/los17/out/target/product/h870/
-mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h870/
-cd /mnt/backup/android/builds/lineage/h870/
-rm lineage_h870-ota*.zip *.md5sum
-sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
+# cd /mnt/backup/android/builds/lineage/h870/
+# rm *.*
+# cd /mnt/backup/android/los17/out/target/product/h870/
+# mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h870/
+# cd /mnt/backup/android/builds/lineage/h870/
+# rm lineage_h870-ota*.zip *.md5sum
+# sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
 echo -e "\nAndroid builds completed and uploaded\n";
 # notify-send Android "Builds uploaded" -i /usr/share/icons/Moka/96x96/apps/android-studio.png
