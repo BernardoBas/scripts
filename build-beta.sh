54c475d358f28dfc0c4929f81be50a04516f443e
Bernardo Bas <bbas@siu.edu.ar>
2021-01-19 12:46:39 -0300

    Make unofficial beta builds for h870 and h872

--- build-beta.sh
+++ build-beta.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-source scripts/sync-lite.sh
+source scripts/sync.sh
 make clean
 export ALLOW_MISSING_DEPENDENCIES=true
 rm -rf packages/apps/Updater/
 brunch h872
-# brunch h870
+brunch h870
 # brunch h870ds
 # brunch us997
 # notify-send Android "Building process finished" -i /usr/share/icons/Moka/96x96/apps/android-studio.png
--- upload-beta.sh
+++ upload-beta.sh
@@ -24,13 +24,13 @@ sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*
 # rm lineage_h870ds-ota*.zip *.md5sum
 # sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
-# cd /mnt/backup/android/builds/lineage/h870/
-# rm *.*
-# cd /mnt/backup/android/los18/out/target/product/h870/
-# mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h870/
-# cd /mnt/backup/android/builds/lineage/h870/
-# rm lineage_h870-ota*.zip *.md5sum
-# sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
+cd /mnt/backup/android/builds/lineage/h870/
+rm *.*
+cd /mnt/backup/android/los18/out/target/product/h870/
+mv *.zip *.md5sum /mnt/backup/android/builds/lineage/h870/
+cd /mnt/backup/android/builds/lineage/h870/
+rm lineage_h870-ota*.zip *.md5sum
+sftp bernardobas@frs.sourceforge.net:/home/pfs/project/lg-g6/Beta/ <<< $'put *.*'
 
 echo -e "\nAndroid builds completed and uploaded\n";
 # notify-send Android "Builds uploaded" -i /usr/share/icons/Moka/96x96/apps/android-studio.png
