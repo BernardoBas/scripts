45d998bece8889bc71f1cdc8c7a0bacf76f4d1e5
Bernardo Bas <bbas@siu.edu.ar>
2021-02-17 19:31:15 -0300

    A11 Builds

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ brunch h870
 # brunch h870ds
 brunch us997
 # notify-send Android "Building process finished" -i /usr/share/icons/Moka/96x96/apps/android-studio.png
-source scripts/upload-beta.sh
+source scripts/upload.sh
